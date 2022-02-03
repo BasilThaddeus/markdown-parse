609f670d03f170a106639ec5ece7ad29ca753598
BasilThaddeus <basiltqsi@gmail.com>
2022-02-02 19:24:51 -0800

    fix to bad line from previous commit

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,6 +16,11 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
 
+            if(markdown.substring(openParen, closeParen).indexOf("\n") != -1) {
+                currentIndex = closeParen + 1;
+                continue;
+            }
+
             if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) break;
             if(nextOpenBracket == 0 || markdown.charAt(nextOpenBracket-1) != '!' && markdown.charAt(openParen - 1) == ']') {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
