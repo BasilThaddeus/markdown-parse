fd1c8d2b3fe2f94b6b84ba714c887d9068a159b9
BasilThaddeus <basiltqsi@gmail.com>
2022-02-02 19:07:07 -0800

    trying to fix last commit

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,9 +16,12 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
 
-            if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) break;
-            if(nextOpenBracket == 0 || markdown.charAt(nextOpenBracket-1) != '!' && markdown.charAt(openParen - 1) == ']') {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(markdown.substring(openParen + 1, closeParen).indexOf(" ") == -1
+            && markdown.substring(openParen + 1, closeParen).indexOf("\n") == -1) {
+                if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) break;
+                if(nextOpenBracket == 0 || markdown.charAt(nextOpenBracket-1) != '!' && markdown.charAt(openParen - 1) == ']') {
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                }   
             }
           
             currentIndex = closeParen + 1;
